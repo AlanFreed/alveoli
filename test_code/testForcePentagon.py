5f5bab0edc232c7249d6c0d699669bcbdd5e4d92
Shahla Zamani <55722766+s-zamani@users.noreply.github.com>
2020-02-19 10:39:37 -0600

    Add files via upload

--- test_code/testForcePentagon.py
+++ test_code/testForcePentagon.py
@@ -19,24 +19,30 @@ author: Prof. Alan Freed, Shahla Zamani
 def run(pentagonGaussPts, state):
 
     h = 0.1
+    # phi is the golden ratio which appears in dodecahedra geometry
+    phi = (1.0 + m.sqrt(5.0)) / 2.0
+    sqrt3 = m.sqrt(3.0)
 
-    # assign the vertices for pentagon 1 in the dodecahedron
-    v1 = vertex(1, m.cos(m.pi/2), m.sin(m.pi/2), 0.0, h)
-    v2 = vertex(2, m.cos(9*m.pi/10), m.sin(9*m.pi/10), 0.0, h)
-    v3 = vertex(3, m.cos(13*m.pi/10), m.sin(13*m.pi/10), 0.0, h)
-    v4 = vertex(4, m.cos(17*m.pi/10), m.sin(17*m.pi/10), 0.0, h)
-    v5 = vertex(5, m.cos(21*m.pi/10), m.sin(21*m.pi/10), 0.0, h)
+
+    # assign the vertices for pentagon 2
+    v2 = vertex(2, 1.0 / sqrt3, 1.0 / sqrt3, -1.0 / sqrt3, h)
+    v3 = vertex(3, -1.0 / sqrt3, 1.0 / sqrt3, -1.0 / sqrt3, h)
+    v10 = vertex(10, 0.0, phi / sqrt3, -1.0 / (sqrt3 * phi), h)
+    v17 = vertex(17, 1.0 / (sqrt3 * phi), 0.0, -phi / sqrt3, h)
+    v18 = vertex(18, -1.0 / (sqrt3 * phi), 0.0, -phi / sqrt3, h)
+
 
     # assign the cords for a pentagon that inscribes an unit circle
     chordGaussPts = 3
-    c1 = chord(1, v5, v1, h, chordGaussPts)
-    c2 = chord(2, v1, v2, h, chordGaussPts)
-    c3 = chord(3, v2, v3, h, chordGaussPts)
-    c4 = chord(6, v3, v4, h, chordGaussPts)
-    c5 = chord(7, v4, v5, h, chordGaussPts)
+    c1 = chord(4, v3, v10, h, chordGaussPts)
+    c2 = chord(3, v2, v10, h, chordGaussPts)
+    c3 = chord(10, v2, v17, h, chordGaussPts)
+    c4 = chord(11, v17, v18, h, chordGaussPts)
+    c5 = chord(12, v3, v18, h, chordGaussPts)
+
 
     # create the pentagon
-    p = pentagon(1, c1, c2, c3, c4, c5, h, pentagonGaussPts)
+    p = pentagon(2, c1, c2, c3, c4, c5, h, pentagonGaussPts)
     p.update()
     p.advance()
     
